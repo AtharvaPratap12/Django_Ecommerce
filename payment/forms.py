0b21cc649d565fdb6f6250c21aae62b675adfb40
Atharva Pratap <atharvapratap0610@gmail.com>
2026-07-24 19:03:37 +0530

    Today worked on the complete checkout page with order summary and shipping information and also added the new page which appears after clicking on the button on the checkout page which takes u to billing page which has a summary of the order and the shipping info and the a small form to fill payment details which will help tio integrate with the paypal or any other things

--- payment/forms.py
+++ payment/forms.py
@@ -17,3 +17,16 @@ class ShippingForm(forms.ModelForm):
         model = ShippingAddress
         fields = ['shipping_full_name','shipping_email','shipping_address1','shipping_address2','shipping_city','shipping_state','shipping_pincode','shipping_country']
         exclude = ['user',]
+
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Name On The Card'}), required = True)
+    card_number = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Card Number'}), required = True)
+    card_exp_date = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Expiration Date'}), required = True)
+    card_cvv_number = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'CVV Number'}), required = True)
+    card_address1 = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Address1'}), required = True)
+    card_address2 = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Address2'}), required = False)
+    card_city = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'City'}), required = True)
+    card_state = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'State'}), required = True)
+    card_pincode = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Pincode'}), required = True)
+    card_country = forms.CharField(label = '', widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Country'}), required = True)
--- payment/views.py
+++ payment/views.py
@@ -1,13 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
+from payment.forms import ShippingForm, PaymentForm
+from payment.models import ShippingAddress
+from django.contrib import messages
 
 # Create your views here.
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html', {"cart_products": cart_products , "quantities": quantities , "totals": totals, "shipping_form":request.POST, "billing_form": billing_form})
+
+
+        else:
+            billing_form = PaymentForm()
+            shipping_form = ShippingForm(request.POST)
+            return render(request, 'payment/billing_info.html', {"cart_products": cart_products , "quantities": quantities , "totals": totals, "shipping_form":shipping_form, "billing_form": billing_form})
+            
+        shipping_form = ShippingForm(request.POST)
+        return render(request, 'payment/billing_info.html', {"cart_products": cart_products , "quantities": quantities , "totals": totals, "shipping_form":shipping_form})
+    else:
+        messages.success(request,"Access Denied!!!!")
+        return redirect('home')
+
 def checkout(request):
     cart = Cart(request)
     cart_products = cart.get_prods
     quantities = cart.get_quants
     totals = cart.cart_total()
-    return render(request, 'payment/checkout.html', {"cart_products": cart_products , "quantities": quantities , "totals": totals})
+
+    if request.user.is_authenticated:
+        # Check if user is logged in User
+        # Shipping User
+        shipping_user = ShippingAddress.objects.get(user__id = request.user.id)
+        # Shipping Form
+        shipping_form = ShippingForm(request.POST or None, instance = shipping_user )
+        return render(request, 'payment/checkout.html', {"cart_products": cart_products , "quantities": quantities , "totals": totals, "shipping_form":shipping_form})
+
+    else:
+        # Check if User is logged ouut
+        shipping_form = ShippingForm(request.POST or None )
+        return render(request, 'payment/checkout.html', {"cart_products": cart_products , "quantities": quantities , "totals": totals, "shipping_form":shipping_form})
     
 
 
